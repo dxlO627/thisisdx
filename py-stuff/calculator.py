7b5a138bfbdaf98e6926cd14c059c4b756f5889f
Rwin-x <178713143+Rwin-X@users.noreply.github.com>
2026-07-28 23:16:50 +0330

    Update calculator.py

--- py-stuff/calculator.py
+++ py-stuff/calculator.py
@@ -12,7 +12,6 @@ def button_click(number):
     e.delete(0, END)
     e.insert(0, str(currect) + str(number))
 
-
 def button_clear():
     e.delete(0, END)
 
@@ -27,7 +26,6 @@ def button_add():
 def button_eq():
     second_number = e.get()
     e.delete(0, END)
-
     if math =="addition":
         e.insert(0, f_num + int(second_number))
     if math =="subtraction":   
@@ -37,8 +35,6 @@ def button_eq():
         e.insert(0, f_num * int(second_number))
     if math =="division":
         e.insert(0, f_num / int(second_number))
-
-
 
 
 def button_subtract():
@@ -70,25 +66,102 @@ def button_divide():
 
 #define buttons
 
-button_1 = Button(root,text="1",padx=40,pady=20,command=lambda :button_click(1))
-button_2 = Button(root,text="2",padx=40,pady=20,command=lambda :button_click(2))
-button_3 = Button(root,text="3",padx=40,pady=20,command=lambda :button_click(3))
-button_4 = Button(root,text="4",padx=40,pady=20,command=lambda :button_click(4))
-button_5 = Button(root,text="5",padx=40,pady=20,command=lambda :button_click(5))
-button_6 = Button(root,text="6",padx=40,pady=20,command=lambda :button_click(6))
-button_7 = Button(root,text="7",padx=40,pady=20,command=lambda :button_click(7))
-button_8 = Button(root,text="8",padx=40,pady=20,command=lambda :button_click(8))
-button_9 = Button(root,text="9",padx=40,pady=20,command=lambda :button_click(9))
-button_0 = Button(root,text="0",padx=40,pady=20,command=lambda :button_click(0))
+button_1 = Button(root,
+                  text="1",
+                  padx=40,
+                  pady=20,
+                  command=lambda :button_click(1))
 
-button_addend = Button(root,text="+",padx=39,pady=20,command=button_add)
-button_equal = Button(root,text="=",padx=91,pady=20,command=button_eq)
-button_clear = Button(root,text="Clear",padx=79,pady=20,command=button_clear)
+button_2 = Button(root,
+                  text="2",
+                  padx=40,
+                  pady=20,
+                  command=lambda :button_click(2))
+
+button_3 = Button(root,
+                  text="3",
+                  padx=40,
+                  pady=20,
+                  command=lambda :button_click(3))
+
+button_4 = Button(root,
+                  text="4",
+                  padx=40,
+                  pady=20,
+                  command=lambda :button_click(4))
+
+button_5 = Button(root,
+                  text="5",
+                  padx=40,
+                  pady=20,
+                  command=lambda :button_click(5))
+
+button_6 = Button(root,
+                  text="6",
+                  padx=40,
+                  pady=20,
+                  command=lambda :button_click(6))
+
+button_7 = Button(root,
+                  text="7",
+                  padx=40,
+                  pady=20,
+                  command=lambda :button_click(7))
+
+button_8 = Button(root,
+                  text="8",
+                  padx=40,
+                  pady=20,
+                  command=lambda :button_click(8))
+
+button_9 = Button(root,
+                  text="9",
+                  padx=40,
+                  pady=20,
+                  command=lambda :button_click(9))
+
+button_0 = Button(root,
+                  text="0",
+                  padx=40,
+                  pady=20,
+                  command=lambda :button_click(0))
+
+button_addend = Button(root,
+                       text="+",
+                       padx=39,
+                       pady=20,
+                       command=button_add)
+
+button_equal = Button(root,
+                      text="=",
+                      padx=91,
+                      pady=20,
+                      command=button_eq)
+
+button_clear = Button(root,
+                      text="Clear",
+                      padx=79,
+                      pady=20,
+                      command=button_clear)
 
 
-button_subtract = Button(root,text="-",padx=41,pady=20,command=button_subtract)
-button_multiply = Button(root,text="*",padx=40,pady=20,command=button_multiply)
-button_divide = Button(root,text="/",padx=41,pady=20,command=button_divide)
+button_subtract = Button(root,
+                         text="-",
+                         padx=41,
+                         pady=20,
+                         command=button_subtract)
+
+button_multiply = Button(root,
+                         text="*",
+                         padx=40,
+                         pady=20,
+                         command=button_multiply)
+
+button_divide = Button(root,
+                       text="/",
+                       padx=41,
+                       pady=20,
+                       command=button_divide)
 
 
 #put the buttons on the screen
